28e14a048bee58043513ff9662fd3905e40f99c4
Yash Patankar <yashrpatankar24@gmail.com>
2026-06-23 18:39:32 +0530

    Add LoginSerializer for user authentication

--- backend/main/accounts/serializers.py
+++ backend/main/accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+
 
 User = get_user_model()
 
@@ -17,3 +19,19 @@ class RegisterSerializer(serializers.ModelSerializer):
             password=validated_data['password']  # automatically hashed
         )
         return user
+
+
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data['email'], password=data['password'])
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password")
+
+        data['user'] = user
+        return data
